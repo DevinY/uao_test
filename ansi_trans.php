d99b28fc1d944c5a455cccbefec319dc84849d81
Devin Yang <devin@ccc.tc>
2018-03-18 16:41:01 +0800

    測試uao文字檔轉utf8

---
+++ ansi_trans.php
@@ -0,0 +1,43 @@
+<?php
+error_reporting(E_ALL);
+ini_set( 'display_errors','1');
+
+    function htu($v){
+        $v = &iconv("big5-hkscs","UTF8", $v);
+        preg_match_all('/&#\\d+;/u', $v,$matches);
+        foreach($matches[0] as $index=>$entitie){
+            $r = mb_convert_encoding($entitie, "UTF-8", "HTML-ENTITIES");
+            if($r!='?'){
+                $v = str_replace($entitie,$r,$v);
+            }
+        }
+        $v=stripslashes($v);
+        return $v;
+    }
+
+	$filename = "ansi_0312.txt";
+	$fp = fopen($filename, "r");
+	$str = fread($fp, filesize($filename));
+
+//uao的array	
+$china_sea_charset = require ("china-sea.php");
+//讀取的文字檔換成16進制
+$hex=bin2hex($str);
+//每4個英文切成一個字
+$chars = explode(",",chunk_split($hex,4,","));
+foreach($chars as $char){
+    $w = hex2bin($char);
+    //嘗試換為UTF-8
+    $word=@iconv("big5", "utf-8//IGNORE",$w);
+    //無法轉換的，視為中國海字集編碼
+    if($word==""){
+	//輸出utf-8字型
+	if(array_key_exists($char,$china_sea_charset)){
+	$text = $china_sea_charset[$char];
+	echo html_entity_decode(sprintf("&#x%s;",$text),ENT_NOQUOTES,"UTF-8");
+	}
+    }else{
+        echo sprintf("%s",$word);
+}
+}
+?>
